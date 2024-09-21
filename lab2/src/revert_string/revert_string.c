e710bc1d72490162846ce0f3011ae43833c2c13a
Vlad Moiseev <57008417+beGGGar@users.noreply.github.com>
2024-09-21 09:42:24 +0000

    закончил 2е задание

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,17 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	char buff;
+	int first = 0;
+	int end = strlen(str) -1;
+	while (first < end){
+		buff = str[first];
+		str[first] = str[end];
+		str[end] = buff;
+		end--;
+		first++;
+	}
 }
 
