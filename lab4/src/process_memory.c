d7c0a489f74d81db6e8bf7c1166580e656afad8f
Vlad Moiseev <57008417+beGGGar@users.noreply.github.com>
2024-11-28 22:09:06 +0000

    process_memory done

--- lab4/src/process_memory.c
+++ lab4/src/process_memory.c
@@ -7,17 +7,22 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-/* Below is a macro definition */
-#define SHW_ADR(ID, I) (printf("ID %s \t is at virtual address: %8X\n", ID, &I))
+/* Below is a macro definition */ //определение макроса
+#define SHW_ADR(ID, I) (printf("ID %s \t is at virtual address: %8X\n", ID, &I)) 
+// #define SHW_ADR(ID, I) (printf("ID %s \t is at virtual address: %p\n", ID, (void*)&I)) // убирает предупреждение о несоответствии типов int и unsigned int 
 
 extern int etext, edata, end; /* Global variables for process
                                  memory */
+
+                                 // etext указывает на конец текстового сегмента программы. может быть использован для определения, где заканчивается код программы
+                                 // edata указывает на конец сегмента данных программы. указывает на первый байт памяти, следующий за последней инициализированной переменной в сегменте данных
+                                 // end указывает на конец сегмента данных или конец всей программы, в зависимости от контекста
 
 char *cptr = "This message is output by the function showit()\n"; /* Static */
 char buffer1[25];
 int showit(); /* Function prototype */
 
-main() {
+int main() {
   int i = 0; /* Automatic variable */
 
   /* Printing addressing information */
@@ -37,12 +42,13 @@ main() {
 } /* end of main function */
 
 /* A function follows */
-int showit(p) char *p;
+int showit(p) char *p; 
 {
   char *buffer2;
   SHW_ADR("buffer2", buffer2);
   if ((buffer2 = (char *)malloc((unsigned)(strlen(p) + 1))) != NULL) {
     printf("Alocated memory at %X\n", buffer2);
+    printf("Address end  : %p \n", (void*)&end);
     strcpy(buffer2, p);    /* copy the string */
     printf("%s", buffer2); /* Didplay the string */
     free(buffer2);         /* Release location */
